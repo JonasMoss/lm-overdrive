177daf1a4cc4854cecb0d12b4a34383e63067a9d
Jonas Moss <jonas.gjertsen@gmail.com>
2018-04-10 15:07:54 +0200

    Fixed and cleaned up functions.

--- R/prior_matching.R
+++ R/prior_matching.R
@@ -1,13 +1,3 @@
-priors_change = function(priors) {
-  prior_list = vector("list", length(priors))
-
-  for(i in 1:length(priors)) {
-    prior_list[[i]] = prior_formula_to_list(priors[[i]])
-  }
-
-  prior_list
-}
-
 #' Make a list from formulas.
 #'
 #' @param formula Two-sided formula with a covariate on the left-hand side and
@@ -19,14 +9,12 @@ priors_change = function(priors) {
 
 prior_formula_to_list = function(formula, .force = TRUE) {
 
-  rhs        = formula[[3]]
-  k          = length(formula[[3]]) - 1 # Number of parameters in the prior.
-  prior_name = deparse(rhs[[1]])
+  call       = formula[[3]]
+  prior_name = deparse(extract_function_name(call))
   formals    = prior_list[[prior_name]]$parameters
 
-  unchecked_params = sapply(1:k, function(i) rhs[[i + 1]])
   checked_params   = check_signature(formals = formals,
-                                     .args   = unchecked_params,
+                                     .args   = extract_arguments(call),
                                      .force  = .force)
 
   list(covariate  = deparse(formula[[2]]),
@@ -39,37 +27,32 @@ prior_formula_to_list = function(formula, .force = TRUE) {
 #' @param priors List of priors.
 #' @return A list containing a matrix of prior parameters and a vector of
 #' prior types.
+
 prior_massage = function(priors) {
-  priors_list = priors_change(priors)
-  mp = max(sapply(priors_list, function(prior) length(prior$parameters)))
+
+  priors_list = lapply(priors, prior_formula_to_list)
+
+  #mp = max(sapply(priors_list, function(prior) length(prior$parameters)))
+  mp = 6
+
   prior_parameters = matrix(0, ncol = mp, nrow = length(priors_list))
 
   for(i in 1:length(priors_list)) {
     prior_parameters[i, 1:length(priors_list[[i]]$parameters)] =
       priors_list[[i]]$parameters
   }
-  rownames(prior_parameters) = sapply(priors_list,
-                                      function(prior) prior$covariate)
-  prior_types = sapply(priors_list, function(prior) {
-    prior_names_to_int(prior$prior)
+
+  rownames(prior_parameters) = sapply(priors_list, function(prior) {
+    prior$covariate
   })
 
-  prior_types_names = sapply(priors_list, function(prior) prior$prior)
-  prior_types = data.frame(type      = prior_types,
-                           name      = prior_types_names)
-  prior_types_covariate = sapply(priors_list, function(prior) prior$covariate)
-  row.names(prior_types) = prior_types_covariate
+  prior_types = sapply(priors_list, function(elem) {
+    prior_list[[elem$prior]]$integer
+  })
+
+  covariates = sapply(priors_list, function(prior) prior$covariate)
+  names(prior_types) = covariates
 
   list(prior_parameters = prior_parameters,
-       prior_types       = prior_types)
+       prior_types      = prior_types)
 }
-
-prior_massage_to_data = function(priors, names) {
-  prior_list = prior_massage(priors)
-  parameters = prior_list$prior_parameters
-  types = prior_list$prior_types
-  list(prior_parameters = parameters[!is.na(match(rownames(parameters), names)), ],
-       prior_types      = types[!is.na(match(rownames(types), names)), ])
-}
-
-priors =

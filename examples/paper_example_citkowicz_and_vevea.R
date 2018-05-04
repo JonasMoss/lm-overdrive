aff58f777767e03ea7c308696f2e3a1e7c26e9c3
JonasMoss <jonas.gjertsen@gmail.com>
2018-05-04 16:00:48 +0200

    :book: Adding Baskerville example data.

---
+++ examples/paper_example_citkowicz_and_vevea.R
@@ -0,0 +1,6 @@
+## =============================================================================
+## Example from "A Parsimonious Weight Function for Modeling Publication Bias"
+## by Martyna Citkowicz and Jack Vevea.
+## =============================================================================
+
+baskerville = readRDS("data/baskerville.Rds")

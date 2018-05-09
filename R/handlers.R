52447c77be58d7e2a917e9aaa83811cfb4ae9aa2
JonasMoss <jonas.gjertsen@gmail.com>
2018-05-09 15:32:35 +0200

    :notes: Added feature to 'coef' and implemented 'confint'.

--- R/handlers.R
+++ R/handlers.R
@@ -11,30 +11,35 @@
 #' @param summary A function for summarizing the posterior samples of the
 #' object. Defaults to \code{mean}.
 #' @param show_effects Logical; If \code{TRUE}, shows the random effects.
+#' @param unlist Logical; If \code{TRUE}, unlists each parameter summary.
 #' @return A list of vectors, one for each formula.
-coef.straussR = function(object, summary = mean, show_effects = FALSE) {
+coef.straussR = function(object, summary = mean, show_effects = FALSE,
+                         unlist = TRUE) {
   stan_data = rstan::extract(object$stan_object)
 
   coefs_names = get_domain(object$formula, object$priors, object$data)
-  coefs = lapply(coefs_names, function(coef) {
-    setNames(rep(0, length(coef)), names(coef))
-    })
+
+  coefs = if(unlist) lapply(coefs_names, function(coef) {
+    stats::setNames(rep(0, length(coef)), names(coef))
+  }) else lapply(coefs_names, function(coef) {
+    stats::setNames(vector("list", length(coef)), names(coef))
+  })
 
   unbounded_index = 0
   positive_index  = 0
   unit_index      = 0
 
-  for(j in 1:length(coefs)) {
-    for(i in 1:length(coefs[[j]])) {
+  for(j in 1:length(coefs_names)) {
+    for(i in 1:length(coefs_names[[j]])) {
       if(coefs_names[[j]][i]== "unbounded") {
         unbounded_index = unbounded_index + 1
-        coefs[[j]][i] = summary(stan_data$beta_unbounded[, unbounded_index])
+        coefs[[j]][[i]] = summary(stan_data$beta_unbounded[, unbounded_index])
       } else if (coefs_names[[j]][i] == "positive") {
         positive_index = positive_index + 1
-        coefs[[j]][i]  = summary(stan_data$beta_positive[, positive_index])
+        coefs[[j]][[i]]  = summary(stan_data$beta_positive[, positive_index])
       } else if (coefs_names[[j]][i] == "unit") {
         unit_index = unit_index + 1
-        coefs[[j]][i] = summary(stan_data$beta_unit[, unit_index])
+        coefs[[j]][[i]] = summary(stan_data$beta_unit[, unit_index])
       }
     }
   }
@@ -46,10 +51,10 @@ print.straussR <- function(x, digits = max(3L, getOption("digits") - 3L),
                            summary = mean,
                            ...) {
 
+  cat("# ======================================================= # \n")
+  cat("# straussR object \n")
+  cat("# ======================================================= # \n \n")
   summary_name = substitute(summary)
-  cat("\nCall:  ",
-      paste(deparse(x$call), sep = "\n", collapse = "\n"), "\n\n", sep = "")
-
   cat("Formula:\n  ")
   form = x$formula
   environment(form) = NULL
@@ -59,7 +64,8 @@ print.straussR <- function(x, digits = max(3L, getOption("digits") - 3L),
   cat("Priors: \n")
   for(i in 1:length(priors)) {
     name = names(priors)[i]
-    cat(" ", name, "\n")
+    cat(" ", name)
+    cat(": \n")
     for(prior_point in priors[[i]]) {
       form = prior_point
       environment(form) = NULL
@@ -75,17 +81,30 @@ print.straussR <- function(x, digits = max(3L, getOption("digits") - 3L),
 
   if(length(coefs) != 0) {
     cat(paste0("Summary of coefficients (", deparse(summary_name), ")"))
-    cat(":\n")
+    cat("\n")
     for(i in 1:length(coefs)) {
       cat("  ")
       cat(names(coefs)[i])
-      cat(":\n")
-      print.default(format(coefs[[i]], digits = digits),
-                    print.gap = 2, quote = FALSE)
+      cat(": \n")
+      print.default(c("", format(coefs[[i]], digits = digits)),
+                    print.gap = 1, quote = FALSE)
     }
   }
+
   invisible(x)
 }
+
+confint.straussR = function(object, parm, level = 0.95) {
+  if(!missing(parm))
+    stop(paste0("The option 'parm' is not implemented; use `[[` or `$` on the ",
+                "returned list instead."))
+  alpha = (1 - level)/2
+  coef(x,
+       summary = function(x) quantile(x, c(alpha, 1 - alpha)),
+       unlist = FALSE)
+
+}
+
 
 
 #' Get the domains
